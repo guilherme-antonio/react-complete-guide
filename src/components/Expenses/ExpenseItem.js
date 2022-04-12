54256b5cd892ade1e20c67323d96a882f10adcb5
guilherme-antonio <guioliveira99@gmail.com>
2022-04-11 23:03:38 -0300

    Creating more specific folders for components

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 
 import ExpenseDate from './ExpenseDate';
-import Card from './Card';
+import Card from '../UI/Card';
 import './ExpenseItem.css';
 
 const ExpenseItem = (props) => {
