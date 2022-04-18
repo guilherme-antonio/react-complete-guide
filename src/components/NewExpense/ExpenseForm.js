4670f5b1b290f3ef9c6ecd0ab96759998b504228
guilherme-antonio <guioliveira99@gmail.com>
2022-04-18 00:01:03 -0300

    Creating a button to show expenses form and cancel button to form

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -66,6 +66,9 @@ const ExpenseForm = (props) => {
         </div>
       </div>
       <div className='new-expense__actions'>
+        <button tpye='button' onClick={props.onCancel}>
+          Cancel
+        </button>
         <button type='submit'>Add Expense</button>
       </div>
     </form>
